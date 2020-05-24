914cc240766eace9631d355dddbea5734ebf4037
Wrzesienski <zhalig@inbox.ru>
2020-05-24 18:42:37 +0300

    - 24.05.2020: - Injury added;

--- Ontology_Lab_3/src/My_Class/Link_Creater.java
+++ Ontology_Lab_3/src/My_Class/Link_Creater.java
@@ -5,6 +5,7 @@ package My_Class;
 
 import My_Class.Handler.Handler;
 import My_Class.Ontology_Name.Injury_type;
+import My_Class.Ontology_Name.Name_Attribut;
 import My_Class.Ontology_Name.Type_Equipment;
 import org.semanticweb.owlapi.model.*;
 
@@ -47,7 +48,11 @@ public class Link_Creater {
         Injury_type.init_map();
         String name = Type_Equipment.get_type_class(Type_Equipment.Type_Class.InjuryType);
 
-        ontology.set_individual_axiom(name, Injury_type.injury_type);
+        for (String injury: Injury_type.injury_type.keySet()){
+            OWLIndividual ind = ontology.set_individual_axiom(name, injury);
+            ontology.set_data_property_axiom(Name_Attribut.get_type_class(Name_Attribut.Attributes.Object_ID),
+                    ind, Long.toString(System.nanoTime()));
+        }
 
     }
 
