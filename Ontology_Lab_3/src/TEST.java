1ccbf79488b4b1949097178b7140eeb8d1a9bf08
Wrzesienski <zhalig@inbox.ru>
2020-06-08 12:52:25 +0300

    - 07.06.2020(super-final version)

--- Ontology_Lab_3/src/TEST.java
+++ Ontology_Lab_3/src/TEST.java
@@ -1,31 +1,12 @@
 import My_Class.Interaction_Ontology;
 import My_Class.Link_Creater;
 import My_Class.Parsing_Ontology;
-import org.apache.jena.datatypes.xsd.XSDDatatype;
-import org.apache.jena.ontology.*;
-import org.apache.jena.rdf.model.*;
-import org.apache.jena.util.FileManager;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.OWLReasoner;
-import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
-import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
-import ru.smarteps.scl.*;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.*;
-import java.util.*;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
-import java.io.FileReader;
-
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import ru.smarteps.scl.SCL;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 
 
 public class TEST {
@@ -35,28 +16,36 @@ public class TEST {
 
 
 //        String Path_XML = "/Users/darkness/Downloads/PIGv10.scd";
-        String Path_XML = "/Users/Alexander/JavaProjects/Ontology_lab_3_4/Ontology_Lab_3/src/resources/PIGv10.scd";
-//        String Path_Ontology = "/Users/darkness/Downloads/Ontology_Lab_3/src/resources/Lab_3_1.owl";
-        String Path_Ontology = "/Users/Alexander/JavaProjects/Ontology_lab_3_4/Ontology_Lab_3/src/resources/Lab_3_1.owl";
+
+
+        String Path_Main = "/Users/darkness/Downloads/Ontology_Lab/";
+        
+        String Path_XML = Path_Main + "src/resources/PIGv10.scd";
+
+        String Path_Ontology = Path_Main + "src/resources/Lab_3_1.owl";
+        String Path_Ret = Path_Main + "src/resources/ontology_new.owl";
+        String Path_Excel = Path_Main + "src/resources/InjuryForParsing.xlsx";
 
         SCL scl = WorkWithConfigurationFiles.unMarshalAny(SCL.class, Path_XML);
 
-        Interaction_Ontology ontology = new Interaction_Ontology(Path_Ontology);
+        Interaction_Ontology ontology = new Interaction_Ontology(Path_Ontology, Path_Excel);
 
 //           парсим
         Parsing_Ontology.parsing(scl, ontology);
 
+        // добавляем виды повреждений
+        Parsing_Ontology.InjuryParsing(ontology);
         ontology.apply_change();
 //
         Link_Creater.Creater(ontology.get_bound(), ontology);
         ontology.apply_change();
 
-        // проблемный кусок
+        // добавляем связи между оборудованием и видами повреждений
         Link_Creater.InjuryCreator(ontology);
         ontology.apply_change();
 
 //        ontology.save_ontology("/Users/darkness/Downloads/Ontology_Lab_3/src/resources/ontology_new.owl");
-        ontology.save_ontology("/Users/Alexander/JavaProjects/Ontology_lab_3_4/Ontology_Lab_3/src/resources/ontology_new.owl");
+        ontology.save_ontology(Path_Ret);
 
 
     }
--- Ontology_Lab_3/src/WorkWithConfigurationFiles.java
+++ Ontology_Lab_3/src/WorkWithConfigurationFiles.java
@@ -4,7 +4,6 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.util.stream.Collectors;
 
 
 public class WorkWithConfigurationFiles {
